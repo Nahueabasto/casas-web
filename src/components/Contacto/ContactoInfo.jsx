e15ff5de9b7b59015c8cba72ed449d244e44b651
Nahueabasto <85427134+Nahueabasto@users.noreply.github.com>
2023-10-02 20:05:54 -0300

    footer hijo de puta

--- src/components/Contacto/ContactoInfo.jsx
+++ src/components/Contacto/ContactoInfo.jsx
@@ -3,6 +3,7 @@ import PhoneIcon from "@mui/icons-material/Phone";
 import EmailIcon from "@mui/icons-material/Email";
 import LocationOnIcon from "@mui/icons-material/LocationOn";
 import "./ContactoInfo.css"
+import PostContacto from "./PostContacto";
 
 
 export default function ContactoInfo(){
--- src/pages/Contacto.jsx
+++ src/pages/Contacto.jsx
@@ -2,13 +2,22 @@ import React from "react";
 import Navbar from "../components/Navbar";
 import ContactoCompleto from "../components/Contacto/ContactoCompleto";
 import Footer from "../components/Footer";
+import ContactoInfo from "../components/Contacto/ContactoInfo";
+import PostContacto from "../components/Contacto/PostContacto";
 
 export default function Contacto(){
     return(
         <div>
-            <div>
+            <div className="conteiner">
                 <Navbar />
-                <ContactoCompleto />
+                <div className="containerr">
+            <div className="contenedor-1">
+                <ContactoInfo />
+            </div>
+            <div className="contenedor-2">
+                <PostContacto />
+            </div>
+            </div>
                 <Footer />
                 </div>
         </div>
